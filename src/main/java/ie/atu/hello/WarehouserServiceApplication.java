dc58b11e1b4a32880be975de8734b6de9330a0a2
StephenHarney <g00367573@atu.ie>
2023-11-08 10:01:39 +0000

    Initial commit

---
+++ src/main/java/ie/atu/hello/WarehouserServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouserServiceApplication.class, args);
+	}
+
+}
